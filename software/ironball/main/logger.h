194d2310e1cdd8aef68682d84be0ee4117af4b35
jucat <lmr2887@163.com>
2026-06-12 23:50:34 +0800

    feat(esp_websocket_client):websocket

---
+++ software/ironball/main/logger.h
@@ -0,0 +1,26 @@
+/**
+ * @copyright Copyright (c) {2022} LMR
+ * @author LMR (17688010148@163.com)
+ * @date 2026-06-12
+ * @brief 
+ */
+
+
+#ifndef _ESP_WEBSOCKET_CLIENT_H_
+#define _ESP_WEBSOCKET_CLIENT_H_
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// 系统日志
+static const char *LOGGER_SYS = "sys";
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- software/ironball/main/main.c
+++ software/ironball/main/main.c
@@ -3,27 +3,33 @@
 #include "nvs_flash.h"
 #include "esp_event.h"
 #include "console_wifi.h"
+#include "esp_log.h"
+#include "logger.h"
+
+
 
 void app_main(void)
 {
+    // 日志 LOGGER_SYS 用 INFO 等级输出
+    ESP_LOGI(LOGGER_SYS, "Ironball! ROLLING!");
+    // 编译器会根据平台将 PRIu32 展开为 u，lu，I32u 等
+    ESP_LOGI(LOGGER_SYS, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
+    ESP_LOGI(LOGGER_SYS, "[APP] IDF version: %s", esp_get_idf_version());
+    // 所有日志都是 INFO 等级，个别日志 "websocket_client"，"transport_ws"，"trans_tcp" 单独设置日志等级
+    esp_log_level_set("*", ESP_LOG_INFO);
+
+    // ESP_ERROR_CHECK 是 ESP 的异常检查，出现异常输出日志并推出程序
+    // 初始化 NVS 存储器，wifi 驱动依赖 NVS 存储
+    ESP_ERROR_CHECK(nvs_flash_init());
+    // 创建网络接口管理器
     ESP_ERROR_CHECK(esp_netif_init());
+    // 创建默认事件循环
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-
-    // Initialize console REPL
+    // 终端交互指令
     ESP_ERROR_CHECK(console_cmd_init());
-
-    // Register wifi commands
+    // 注册 wifi 交互指令
     ESP_ERROR_CHECK(console_cmd_wifi_register());
-
-    // Start console REPL
+    // 启动终端交互
     ESP_ERROR_CHECK(console_cmd_start());
 
-    printf("Ironball! ROLLING!\n");
 }
